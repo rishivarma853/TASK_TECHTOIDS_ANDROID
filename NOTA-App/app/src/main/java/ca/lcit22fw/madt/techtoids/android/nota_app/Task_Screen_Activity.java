f385ece715ede57c0ae39d3f2b6ca93268a33b8b
simran mehra <simranmehra.iv@gmail.com>
2023-02-16 18:07:19 -0500

    Creating the Task Screen

---
+++ NOTA-App/app/src/main/java/ca/lcit22fw/madt/techtoids/android/nota_app/Task_Screen_Activity.java
@@ -0,0 +1,14 @@
+package ca.lcit22fw.madt.techtoids.android.nota_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Task_Screen_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_screen);
+    }
+}
