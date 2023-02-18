1253e3f2ec4d1866bbf8eaaa07ddf91f48682c20
simran mehra <simranmehra.iv@gmail.com>
2023-02-17 20:44:15 -0500

    Added custom dialog box

--- NOTA-App/app/src/main/java/ca/lcit22fw/madt/techtoids/android/nota_app/HomeScreenActivity.java
+++ NOTA-App/app/src/main/java/ca/lcit22fw/madt/techtoids/android/nota_app/HomeScreenActivity.java
@@ -3,6 +3,7 @@ package ca.lcit22fw.madt.techtoids.android.nota_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -20,7 +22,8 @@ public class HomeScreenActivity extends AppCompatActivity {
 
     GridView simpleGrid;
     FloatingActionButton fltbtnAddNewBoard;
-//    int[] whiteboardImg = {R.drawable.whiteboard,R.drawable.whiteboard};
+
+    //    int[] whiteboardImg = {R.drawable.whiteboard,R.drawable.whiteboard};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,30 +46,57 @@ public class HomeScreenActivity extends AppCompatActivity {
         fltbtnAddNewBoard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder alert = new AlertDialog.Builder(HomeScreenActivity.this);
-                LayoutInflater inflater = HomeScreenActivity.this.getLayoutInflater();
-
-                alert.setTitle("Adding a new board ");
-                alert.setMessage("Enter Name");
-                 EditText input = new EditText(HomeScreenActivity.this);
-                alert.setView(input);
-
-                alert.setPositiveButton("ADD", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int whichButton) {
-                                String value = String.valueOf(input.getText());
-                                // Do something with value!
-                            }
-                }).setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-
-                            }
-                        })
-                        .create().show();
-
-
+                showDialog();
+            }
+        });
 
     }
-});
-    }}
+//        fltbtnAddNewBoard.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                AlertDialog.Builder alert = new AlertDialog.Builder(HomeScreenActivity.this);
+//                LayoutInflater inflater = HomeScreenActivity.this.getLayoutInflater();
+//
+//                alert.setTitle("Adding a new board ");
+//                alert.setMessage("Enter Name");
+//                 EditText input = new EditText(HomeScreenActivity.this);
+//                alert.setView(input);
+//
+//                alert.setPositiveButton("ADD", new DialogInterface.OnClickListener() {
+//                            public void onClick(DialogInterface dialog, int whichButton) {
+//                                String value = String.valueOf(input.getText());
+//                                // Do something with value!
+//                            }
+//                }).setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                dialog.dismiss();
+//
+//                            }
+//                        })
+//                        .create().show();
+//
+//
+//
+//    }
+//});
+//    }
+    
+        private void showDialog () {
+            Dialog dialog = new Dialog(this, R.style.DialogStyle);
+            dialog.setContentView(R.layout.customdialogbox);
+
+            dialog.getWindow().setBackgroundDrawableResource(R.drawable.bg_window);
+
+            ImageView btnClose = dialog.findViewById(R.id.btn_close);
+
+            btnClose.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog.dismiss();
+                }
+            });
+
+            dialog.show();
+        }
+    }
